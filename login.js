9d691aa1dd8bf1f8284f8a7b593642d1cb769ddf
hubertchen200 <hubertchen200@gmail.com>
2023-11-08 22:06:52 -0500

    change password

--- login.js
+++ login.js
@@ -9,9 +9,9 @@ document.addEventListener("DOMContentLoaded", function() {
         const password = document.getElementById("password").value;
 
         // Replace this with your authentication logic
-        if (username === "yourusername" && password === "yourpassword") {
+        if (username === "1" && password === "1") {
             // Redirect to the home page (replace with your actual URL)
-            window.location.href = "home.html";
+            window.location.href = "index.html";
         } else {
             errorMessage.innerText = "Invalid username or password. Please try again.";
         }
