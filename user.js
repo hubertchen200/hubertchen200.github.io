a0c42f9915bcd0da8e3329b4006d5dc6d719c682
Hubert Chen <hubertchen@Huberts-iMac.hsd1.nj.comcast.net>
2024-07-15 15:29:00 -0400

    init commit for chat ui

---
+++ user.js
@@ -0,0 +1,33 @@
+document.getElementById('signup-form').addEventListener('submit', async function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const email = document.getElementById('email').value;
+    const firstname = document.getElementById('firstname').value;
+    const lastname = document.getElementById('lastname').value;
+  
+    const response = await fetch('https://user.hubertchen200.site/user', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization':'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiaGkiLCJ1c2VybmFtZSI6ImhpIiwiZXhwIjoxNzIwODI3MTc4fQ.ghjPDVhMXRAmORvSmbgKdLKeOb2vSU6_mXuLkXuFhq0'
+        },
+        body: JSON.stringify({"email": email, "password": password, "username": username, "firstname": firstname, "lastname": lastname })
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+        // if ("status" in data) {
+            alert('Sign-up successful');
+
+    //     } else {
+    //         alert('sign up failed!');
+    //     }
+    }
+            
+    else {
+        alert('Sign-up failed: ' + data.message);
+    }
+});
